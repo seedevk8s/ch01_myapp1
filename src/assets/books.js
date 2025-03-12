30c3f5debc04eaff3949f6c210cee8dd16fe32b5
seedevk8s <seedevk8s@gmail.com>
2025-03-12 11:34:51 +0900

    (6) 더미 데이터 작성과 이미지 데이터 준비

---
+++ src/assets/books.js
@@ -0,0 +1,42 @@
+const data = [
+  {
+      title: "코드로 배우는 스프링 부트 웹 프로젝트",
+      cate: "IT/개발 실무",
+      pubdate: "2020-12-25",
+      pages: 644,
+      sug: 0,
+      price: 34000,
+      picUrl: "./assets/springboot.jpg",
+   },
+   {
+      title: "코드로 배우는 리액트",
+      cate: "IT/개발 실무",
+      pubdate: "2023-12-01",
+      pages: 592,
+      sug: 0,
+      price: 35000,
+      picUrl: "./assets/react.jpg",
+   },
+   {
+      title: "텐서플로로 배우는 수치최적화와 딥러닝",
+      cate: "IT/개발 기본",
+      pubdate: "2022-11-03",
+      pages: 614,
+      sug: 0,
+      price: 30000,
+      picUrl: "./assets/tensor.jpg",
+   },
+   {
+      title: "코드를 통해 본 빵형의 실전 Java",
+      cate: "IT/개발 기본",
+      pubdate: "2020-06-05",
+      pages: 816,
+      sug: 0,
+      price: 32000,
+      picUrl: "./assets/code.jpg",
+   },
+];
+
+const cates = ["자격증 수험서", "IT/개발 기본", "IT/개발 실무", "디자인", "취미"];
+
+export { data, cates };
